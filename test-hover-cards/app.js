1bd1c32c88664b961a9b2208bfbd304e092db3da
Mirko <ruhlmirkojoel@gmail.com>
2025-03-04 01:53:21 -0300

    Working version

--- test-hover-cards/app.js
+++ test-hover-cards/app.js
@@ -1,1 +1,12 @@
-"use strict";
+const cards = document.querySelectorAll(".card");
+const wrapper = document.querySelector(".cards");
+
+wrapper.addEventListener("mousemove", function (event) {
+  cards.forEach((card) => {
+    const rect = card.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+
+    card.style.background = `radial-gradient(960px circle at ${x}px ${y}px, rgba(133, 59, 251), transparent 15%)`;
+  });
+});
