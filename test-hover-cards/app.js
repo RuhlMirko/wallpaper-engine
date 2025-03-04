fa57e333c88ce78e1d75d3bdf274ba4e62e7d26a
Mirko <ruhlmirkojoel@gmail.com>
2025-03-04 04:53:22 -0300

    Added rgba sliders to register initial rgba values

--- test-hover-cards/app.js
+++ test-hover-cards/app.js
@@ -1,5 +1,7 @@
 const cards = document.querySelectorAll(".card");
 const wrapper = document.querySelector(".cards");
+const currentRgba = document.querySelector(".picker-container input").value;
+const userInput = document.querySelectorAll(".picker-preview input");
 
 wrapper.addEventListener("mousemove", function (event) {
   cards.forEach((card) => {
@@ -11,4 +13,17 @@ wrapper.addEventListener("mousemove", function (event) {
     rgba(133, 59, 251),
     transparent 15%)`;
   });
+});
+
+const values = currentRgba
+  .match(/\(([^)]+)\)/)[1]
+  .split(",")
+  .map(Number);
+
+values.forEach((val, index) => {
+  if (index == 3) {
+    userInput[index].value = val * 100;
+  } else {
+    userInput[index].value = val;
+  }
 });
