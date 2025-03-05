cfa075f0ab843e1ecd6be448d3c6228f2725038d
Mirko <ruhlmirkojoel@gmail.com>
2025-03-05 02:13:58 -0300

    added background and tracking mouse movement

---
+++ test-trackNshoot/app.js
@@ -0,0 +1,16 @@
+let box = document.querySelector(".box");
+let boxBoundingRect = box.getBoundingClientRect();
+
+let boxCenter = {
+  x: boxBoundingRect.left + boxBoundingRect.width / 2,
+  y: boxBoundingRect.top + boxBoundingRect.height / 2,
+};
+document.addEventListener("mousemove", (e) => {
+  let angle =
+    Math.atan2(e.pageX - boxCenter.x, -(e.pageY - boxCenter.y)) *
+    (180 / Math.PI);
+
+  //let angle = Math.atan2(e.pageX, -e.pageY * (180 / Math.PI));
+  box.style.transform = `rotate(${angle}deg)`;
+  console.log(Math.floor(angle));
+});
