5919e181e606c0ae7e526ecfed17a24bbdaead6f
Mirko <ruhlmirkojoel@gmail.com>
2025-03-05 12:27:45 -0300

    Added the chargeback explosion effect

--- test-trackNshoot/app.js
+++ test-trackNshoot/app.js
@@ -9,8 +9,6 @@ document.addEventListener("mousemove", (e) => {
   let angle =
     Math.atan2(e.pageX - boxCenter.x, -(e.pageY - boxCenter.y)) *
     (180 / Math.PI);
-
-  //let angle = Math.atan2(e.pageX, -e.pageY * (180 / Math.PI));
   box.style.transform = `rotate(${angle}deg)`;
   console.log(Math.floor(angle));
 });
