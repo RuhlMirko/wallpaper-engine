3aae5e3d6240ca2a4ac8c1d289be79f6f92a790e
Mirko <ruhlmirkojoel@gmail.com>
2025-03-06 05:03:01 -0300

    Added projectile sprite

--- test-trackNshoot/app.js
+++ test-trackNshoot/app.js
@@ -1,16 +1,19 @@
-let box = document.querySelector(".box");
-let boxBoundingRect = box.getBoundingClientRect();
+let maurice = document.querySelector(".maurice");
+let projectile = document.querySelector(".projectile");
+//projectile.style.transform = `rotate(${angle - 90}deg)`;
+let boxBoundingRect = maurice.getBoundingClientRect();
 
 let boxCenter = {
   x: boxBoundingRect.left + boxBoundingRect.width / 2,
   y: boxBoundingRect.top + boxBoundingRect.height / 2,
 };
+
+console.log(boxCenter.x, boxCenter.y);
 document.addEventListener("mousemove", (e) => {
   let angle =
     Math.atan2(e.pageX - boxCenter.x, -(e.pageY - boxCenter.y)) *
     (180 / Math.PI);
-  box.style.transform = `rotate(${angle}deg)`;
-  console.log(Math.floor(angle));
+  maurice.style.transform = `rotate(${angle}deg)`;
 });
 
 // TODO: make maurice reset its tracking after window resize
