2121205ee29bbe8841bb04e7f467083775bd3a44
Mirko <ruhlmirkojoel@gmail.com>
2025-03-09 22:25:27 -0300

    Added code from shoot ing to the in progress wallpaper

--- test-trackNshoot/app.js
+++ test-trackNshoot/app.js
@@ -2,28 +2,53 @@ let maurice = document.querySelector(".maurice");
 let projectile = document.querySelector(".projectile");
 //projectile.style.transform = `rotate(${angle - 90}deg)`;
 let boxBoundingRect = maurice.getBoundingClientRect();
+const logCons = document.getElementById("log");
 
 let boxCenter = {
   x: boxBoundingRect.left + boxBoundingRect.width / 2,
   y: boxBoundingRect.top + boxBoundingRect.height / 2,
 };
 
-console.log(boxCenter.x, boxCenter.y);
-document.addEventListener("mousemove", (e) => {
+var bullet = {
+  x: boxCenter.x,
+  y: boxCenter.y,
+  dx: 0.0,
+  dy: 0.0,
+  radius: 5.0,
+
+  tick: function () {
+    this.x += this.dx;
+    this.y += this.dy;
+
+    if (
+      this.x + this.radius < 0.0 ||
+      this.x - this.radius > canvasWidth ||
+      this.y + this.radius < 0.0 ||
+      this.y - this.radius > canvasHeight
+    ) {
+      this.dx = 0.0;
+      this.dy = 0.0;
+    }
+  },
+};
+
+window.onload = function () {
+  loop();
+};
+
+window.onmousemove = function (e) {
   let angle =
     Math.atan2(e.pageX - boxCenter.x, -(e.pageY - boxCenter.y)) *
     (180 / Math.PI);
   maurice.style.transform = `rotate(${angle}deg)`;
 
-  const animationTime = 2500;
-
   projectile.style.transform = `rotate(${angle - 90}deg)`;
-  let targetCoords = [`${e.pageX - 300}px`, `${e.pageY - 30}px`]; // Saves target coordinates
+  // Saves target coordinates (Dont know why it put minus 300 and minus 30)
+  //let targetCoords = [`${e.pageX - 300}px`, `${e.pageY - 30}px`];
+};
 
-  /*
-  setTimeout(() => {
-    projectile.remove();
-  }, animationTime);*/
-});
+window.onmousedown = function (e) {
+  logCons.innerHTML = `${e.pageY}, ${e.pageX} `;
+};
 
 // TODO: make maurice reset its tracking after window resize
