910a7201fa1cf857b0c9ebc8f3a5aee6134d6c7e
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2025-08-22 22:18:22 -0300

    Added name of the day to first array

--- codeTime/app.js
+++ codeTime/app.js
@@ -1,7 +1,18 @@
 "use strict";
 
+const dayNames = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
 const hourEl = document.getElementById("currHour");
 const dayEl = document.getElementById("currDay");
+let dayProgress = 0.0;
 
 setInterval(() => {
   const now = new Date();
@@ -9,13 +20,18 @@ setInterval(() => {
   const minutes = String(now.getMinutes()).padStart(2, "0");
   const seconds = String(now.getSeconds()).padStart(2, "0");
 
+  const date = now.getDate();
   const day = now.getDay();
   const month = now.getMonth();
   const year = now.getFullYear();
-  console.log(day, month, year);
 
-  const fullHour = `[${hours}, ${minutes}, ${seconds}]`;
+  const fullHour = `[ ${hours}, ${minutes}, ${seconds} ],`;
   hourEl.textContent = fullHour;
-  const fullDate = `[${month}, ${day}, ${year}]`;
+  const totalMinutes = 24 * 60; // 1440 minutes in a day
+  const currentMinutes = parseInt(hours) * 60 + parseInt(minutes);
+  dayProgress = ((currentMinutes / totalMinutes) * 100).toFixed(2);
+  console.log(dayProgress);
+
+  const fullDate = `[ ${month}, ${date}, ${year}, "${dayNames[day]}" ],`;
   dayEl.textContent = fullDate;
 }, 1000);
