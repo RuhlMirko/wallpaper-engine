4a1979068931b47fcc5123bbd70c57d4e68aa47a
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2025-08-23 01:34:10 -0300

    Background display different color depending on the time of day

--- codeTime/app.js
+++ codeTime/app.js
@@ -10,8 +10,19 @@ const dayNames = [
   "Saturday",
   "Sunday",
 ];
+const stages = {
+  dawn: "linear-gradient(to top, rgba(240, 155, 65, 1) 0%, rgba(59, 162, 196, 1) 46%, rgba(55, 133, 189, 1) 76%)",
+  afternoon:
+    "linear-gradient(0deg,rgba(110, 180, 204, 1) 46%, rgba(78, 167, 230, 1) 76%)",
+  sunset:
+    "linear-gradient(0deg,rgba(170, 87, 102, 0.94) 3%, rgba(245, 192, 118, 1) 33%, rgba(247, 85, 85, 1) 100%)",
+  night:
+    "linear-gradient(0deg,rgba(10, 12, 17, 1) 0%, rgba(26, 26, 43, 1) 100%)",
+};
+
 const hourEl = document.getElementById("currHour");
 const dayEl = document.getElementById("currDay");
+const background = document.getElementById("bg");
 let dayProgress = 0.0;
 
 setInterval(() => {
@@ -34,4 +45,10 @@ setInterval(() => {
 
   const fullDate = `[ ${month}, ${date}, ${year}, "${dayNames[day]}" ],`;
   dayEl.textContent = fullDate;
+  if (dayProgress > 80 || dayProgress <= 25)
+    background.style.background = stages.night;
+  else if (dayProgress >= 20 && dayProgress < 50)
+    background.style.background = stages.dawn;
+  else if (dayProgress >= 50) background.style.background = stages.afternoon;
+  else background.style.background = stages.sunset;
 }, 1000);
